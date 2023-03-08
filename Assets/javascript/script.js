2ba15f0443f72d6aee58260859802020d10d94a7
Alicia Keberle <122774607+keberlea@users.noreply.github.com>
2023-03-07 20:20:33 -0800

    added html and css

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -2,3 +2,9 @@ var APIkey = "9b9d5a244d38b072822e7b010a887234"
 var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid={9b9d5a244d38b072822e7b010a887234}"
 
 fetch(queryURL)
+.then(function(response){
+    return response.json();
+})
+.then(function(data){
+    console.log(data);
+})
