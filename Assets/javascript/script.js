f2a438987699f4f1e09cd210384d3999712badb4
Alicia Keberle <122774607+keberlea@users.noreply.github.com>
2023-03-07 21:04:43 -0800

    added fetch for java

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,10 +1,17 @@
-var APIkey = "9b9d5a244d38b072822e7b010a887234"
-var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid={9b9d5a244d38b072822e7b010a887234}"
+var weatherAPIKey = "9b9d5a244d38b072822e7b010a887234"
 
-fetch(queryURL)
-.then(function(response){
-    return response.json();
-})
-.then(function(data){
-    console.log(data);
-})
+
+
+//
+var weatherSearch =  function(cityname){
+    var urlCurrentWeather="https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${weatherAPIKey}&units=imperial";
+    //fetch current weather
+    fetch(urlCurrentWeather)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (currentData){
+        console.log(currentData);
+    })
+
+}
