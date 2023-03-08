86509f5c3a3befb17114d1767a1d81ba90fc7642
Alicia Keberle <122774607+keberlea@users.noreply.github.com>
2023-03-07 19:27:30 -0800

    added head to html

---
+++ Assets/javascript/script.js
@@ -0,0 +1,4 @@
+var APIkey = "9b9d5a244d38b072822e7b010a887234"
+var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid={9b9d5a244d38b072822e7b010a887234}"
+
+fetch(queryURL)
