fd8e64a7f08676c84763db047541e053cf9cff5a
Alicia Keberle <122774607+keberlea@users.noreply.github.com>
2023-03-09 11:34:12 -0800

    took off uv index, added city search function and search eventlistener

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -2,7 +2,12 @@ var searchFormEl = document.querySelector(".form");
 var cityInputEl = document.quesrySelector(".city-input")
 var searchButtonEl = document.querySelector(".search-btn");
 
-
+//current forcast variables
+var tempForecastEl = document.querySelector(".temp");
+var windForecastEl = document.querySelector(".wind");
+var humidityForecastEl = document.querySelector(".humidity");
+var weatherResultsEl = document.querySelector(".weather-results");
+var currentConditionsEl = document.querySelector(".current-condition")
 var weatherAPIKey = "9b9d5a244d38b072822e7b010a887234"
 var cityList = [];
 
@@ -32,7 +37,39 @@ var weatherSearch =  function(cityname){
                 cityResultEl.innerHTML = currentData.name + dayjs().format(MM/DD/YYY) +
                 `<img class="w-2"
                 src="http://openweathermap.org/img/wn/${currentData.weather[0].icon}@2x.png" />`;
-            })
-    })
+                tempForecastEl.innerHTML = "Temp:" + fivedayData.current.temp + "°F";
+                windForecastEl.innerHTML = "Wind:" + fivedayData.current.wind_speed;
+                humidityForecastEl.innerHTML = "Humidity" + fivedayData.current.humidity;
+                //current city 5-day forecast
+                fiveDayEl.innerHTML = "";
+          var card = "";
+          for (let i = 1; i < 6; i++) {
+            console.log(fivedayData.daily[i].weather[0].icon);
+            card =
+              card +
+              `<div class="col-sm-2">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">${dayjs(fivedayData.daily[i].dt,"X").format("MM/DD/YYYY")}</h5>
+                            <img class="w-50" src="http://openweathermap.org/img/wn/${fivedayData.daily[i].weather[0].icon}@2x.png" />
+                            <p class="card-text">Temp: ${fivedayData.daily[i].temp.day}°F</p>
+                            <p class="card-text">Wind: ${fivedayData.daily[i].wind_speed}</p>
+                            <p class="card-text">Humidity: ${fivedayData.daily[i].humidity}</p>
+                        </div>
+                    </div>
+                </div>`;
+          }
+          fiveDayEl.innerHTML = card;
+        });
+    });
+};
 
-}
+// Function to search for a city
+var citySearch = function () {
+    // obtain input from user
+    var cityName = cityInputEl.value;
+    weatherSearch(cityName);
+  };
+  
+  // event listener for the search button click
+  searchButtonEl.addEventListener("click", citySearch);
